ad1cc6bf877d5283feab9c6ce74aa1ea23cb2724
HeewonJo <heewonjo.me@gmail.com>
2024-11-20 16:56:31 +0900

    Feat: 게임 진행 관련 각종 기능 추가

--- project/games/urls.py
+++ project/games/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('house/<int:game_id>/', views.select_house, name='select_house'),
     path('<int:month>/<int:time>/', views.game_start, name='game_start'),
     path('night/<int:month>/', views.night_transition, name='night_transition'),
-    # path('restaurant/<int:game_id>/<str:restaurant_name>/', views.restaurant_detail, name='restaurant_detail'),
+    path('restaurant_detail/<int:game_id>/<str:restaurant_name>/', views.restaurant_detail, name='restaurant_detail'),
     path('hospital/<int:game_id>/', views.hospital_visit, name='hospital_visit'),
     path('cooking-result/<int:game_id>/', views.cooking_result, name='cooking_result'),
     path('<int:game_id>/', views.game_ending, name='game_ending'),
+    path('fail/<int:game_id>/', views.game_fail, name="game_fail"),
 ]
